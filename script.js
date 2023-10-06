e4b3b3a529977241b066b38512ea5308a77fb1b9
Vishal Singh Rawat <113826239+Vishalsinghrawat7535@users.noreply.github.com>
2023-10-06 20:41:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// let p = fetch ("https://api.1forge.com/quotes?pairs=EUR/USD,GBP/JPY,AUD/USD&api_key=YOUR_API_KEY")
+
+// p.then ((value1) =>{
+//     console.log(value1.status)
+// return value1.json()
+// }).then ((value2)=> {
+//     console.log(value2)
+// })  
